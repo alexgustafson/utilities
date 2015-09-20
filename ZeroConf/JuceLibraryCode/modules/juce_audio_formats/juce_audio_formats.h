bf26dbeb7bbecd412c5daff83bfafa530039fe8d
alexgustafson <alex_gustafson@yahoo.de>
2015-09-20 20:55:29 +0200

    audio processor node added

--- ZeroConf/JuceLibraryCode/modules/juce_audio_formats/juce_audio_formats.h
+++ ZeroConf/JuceLibraryCode/modules/juce_audio_formats/juce_audio_formats.h
@@ -1,5 +1,15 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../../../JUCE/modules/juce_audio_formats/juce_audio_formats.h"
+#if defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../../../../JUCE/modules/juce_audio_formats/juce_audio_formats.h"
+#elif defined (JUCER_XCODE_IPHONE_5BC26AE3)
+ #include "../../../..../../../JUCE/modules/juce_audio_formats/juce_audio_formats.h"
+#elif defined (JUCER_LINUX_MAKE_6D53C8B4)
+ #include "../../../../../../JUCE/modules/juce_audio_formats/juce_audio_formats.h"
+#elif defined (JUCER_CODEBLOCKS_LINUX_191E0897)
+ #include "../../../../../../JUCE/modules/juce_audio_formats/juce_audio_formats.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
 
